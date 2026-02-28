99c7a3478f5a99e47c904e5318a758947c2334a8
Abhilash Tripathy <abhilashtripathy25@gmail.com>
2026-02-28 23:43:13 +0530

    Added base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/ganavi-portfolio/",
   plugins: [react()],
 });
